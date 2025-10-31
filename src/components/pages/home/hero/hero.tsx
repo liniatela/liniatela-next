1edd91cef79066349255bef42127f66e68a04be5
Mikhail Elistratov <mikhail.elistratov@pixelpoint.io>
2025-10-31 13:15:43 +0700

    feat: добавление компонента PauseableVideo для воспроизведения видео в секции Hero, обновление зависимостей и добавление новых видеофайлов

--- src/components/pages/home/hero/hero.tsx
+++ src/components/pages/home/hero/hero.tsx
@@ -1,32 +1,34 @@
 import Image from 'next/image'
 
 import backgroundImage from './images/background.jpg'
+import PauseableVideo from '@/components/shared/pauseable-video'
 
 function Hero() {
-	return (
-		<section className='hero relative h-[105vh] overflow-hidden '>
-			<div className='container h-full flex flex-col justify-center'>
-				<div className='text-white space-y-3'>
-					<p className='text-2xl lg:text-3xl tracking-tighter text-[#D8D8D8]'>
-						Твоя гибкость — <br /> Это свобода быть собой{' '}
-					</p>
-					<h1 className='text-[40px] lg:text-[80px] leading-none tracking-tighter'>Студия мягкого фитнеса</h1>
-				</div>
-			</div>
-			<div className="absolute inset-0 -z-10 w-full h-full pointer-events-none">
-				<Image
-					className="w-full h-full object-cover object-bottom"
-					src={backgroundImage}
-					alt=""
-					width={1920}
-					height={800}
-					priority
-					sizes="(max-width: 640px) 100vw, (max-width: 1024px) 100vw, 1920px"
-				/>
-				<div className="absolute inset-0 bg-black/60" aria-hidden="true" />
-			</div>
-		</section>
-	)
+  return (
+    <section className='hero relative h-[105vh] overflow-hidden '>
+      <div className='container h-full flex flex-col justify-center'>
+        <div className='text-white space-y-3'>
+          <p className='text-2xl lg:text-3xl tracking-tighter text-[#D8D8D8]'>
+            Твоя гибкость — <br /> Это свобода быть собой{' '}
+          </p>
+          <h1 className='text-[40px] lg:text-[80px] leading-none tracking-tighter'>Студия мягкого фитнеса</h1>
+        </div>
+      </div>
+      <div className="absolute inset-0 -z-10 w-full h-full pointer-events-none">
+        <PauseableVideo
+          className="absolute h-full w-full"
+          videoClassName="h-full w-full object-cover"
+          width={1920}
+          height={1080}
+          poster={backgroundImage.src}
+        >
+          <source src='/videos/hero/background.mp4' type="video/mp4" />
+          <source src="/videos/pages/home/hero/background.webm" type="video/webm" />
+        </PauseableVideo>
+        <div className="absolute inset-0 bg-black/60" aria-hidden="true" />
+      </div>
+    </section>
+  )
 }
 
 export default Hero
---
+++ src/components/shared/pauseable-video/pauseable-video.tsx
@@ -0,0 +1,165 @@
+'use client';
+
+import Image from 'next/image';
+
+import {
+  ForwardRefRenderFunction,
+  PropsWithChildren,
+  forwardRef,
+  useCallback,
+  useEffect,
+  useImperativeHandle,
+  useRef,
+  useState,
+} from 'react';
+import { useInView } from 'react-intersection-observer';
+
+import { AnimatePresence, LazyMotion, domAnimation, m } from 'framer-motion';
+
+
+
+import { cn } from '@/lib/utils';
+
+// Example of ffmpeg optimization parameters:
+// mp4: -c:v libx265 -crf 20 -vf scale=3840:-2 -preset veryslow -tag:v hvc1 -movflags faststart -an
+// webm: -c:v libvpx-vp9 -crf 20 -vf scale=3840:-2 -deadline best
+
+type PauseableVideoProps = {
+  className?: string;
+  videoClassName?: string;
+  customInView?: boolean;
+  width: number;
+  height: number;
+  withReset?: boolean;
+  withLoop?: boolean;
+  poster?: string;
+};
+
+/* eslint-disable react/function-component-definition */
+const PauseableVideo: ForwardRefRenderFunction<
+  HTMLVideoElement | null,
+  PropsWithChildren<PauseableVideoProps>
+> = (
+  {
+    children,
+    className,
+    videoClassName,
+    width,
+    height,
+    customInView,
+    withReset = false,
+    withLoop = true,
+    poster,
+  },
+  ref,
+) => {
+  const videoRef = useRef<HTMLVideoElement>(null);
+  const [isVideoLoaded, setIsVideoLoaded] = useState(false);
+
+  const [videoVisibilityRef, isInView] = useInView({
+    triggerOnce: true,
+    rootMargin: '400px 0px',
+  });
+  const { inView, ref: setVideoRef } = useInView({ threshold: 0.1 });
+
+  useEffect(() => {
+    setVideoRef(videoRef.current);
+  }, [setVideoRef]);
+
+  const playVideo = useCallback(
+    (videoElement: HTMLVideoElement) => {
+      if (withReset) {
+        videoElement.currentTime = 0;
+      }
+      const playPromise = videoElement.play();
+
+      if (playPromise !== undefined) {
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        playPromise.catch((error: any) => {
+          // eslint-disable-next-line no-console
+          console.error('Error attempting to play video:', error);
+        });
+      }
+    },
+    [withReset],
+  );
+
+  useEffect(() => {
+    if (!videoRef.current) {
+      return;
+    }
+
+    const videoElement = videoRef.current;
+
+    if (customInView !== undefined) {
+      if (customInView) {
+        playVideo(videoElement);
+      } else {
+        videoElement.pause();
+      }
+    } else {
+      if (inView) {
+        playVideo(videoElement);
+      } else {
+        videoElement.pause();
+      }
+    }
+  }, [inView, customInView, playVideo]);
+
+  // Combine the external ref with the internal videoRef
+  useImperativeHandle<HTMLVideoElement | null, HTMLVideoElement | null>(
+    ref,
+    () => videoRef.current,
+  );
+
+  return (
+    <>
+      {poster && !isVideoLoaded && (
+        <div className={cn(className, videoClassName, 'absolute bg-page-background')}>
+          <Image
+            src={poster}
+            alt="Video poster"
+            width={width}
+            height={height}
+            className={cn('h-full w-full object-cover', videoClassName)}
+            quality={90}
+            priority
+          />
+        </div>
+      )}
+
+      <LazyMotion features={domAnimation}>
+        <div className={className} ref={videoVisibilityRef}>
+          <AnimatePresence>
+            {isInView && (
+              <m.video
+                className={cn('absolute inset-0', videoClassName)}
+                ref={videoRef}
+                controls={false}
+                width={width}
+                height={height}
+                {...(!poster && {
+                  initial: {
+                    opacity: 0,
+                  },
+                  animate: { opacity: isVideoLoaded ? 1 : 0 },
+                  transition: { duration: 0.5 },
+                })}
+                loop={withLoop}
+                style={{ display: isVideoLoaded ? 'block' : 'none' }}
+                muted
+                autoPlay
+                playsInline
+                onLoadedData={() => setIsVideoLoaded(true)}
+              >
+                {children}
+              </m.video>
+            )}
+          </AnimatePresence>
+        </div>
+      </LazyMotion>
+    </>
+  );
+};
+
+export default forwardRef(PauseableVideo);
